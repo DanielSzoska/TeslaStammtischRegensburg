7dbd10b0f4e9694e42be349432e74d1546be4959
Felix Drescher-Hackel <felix@drescher-hackel.de>
2025-07-26 02:02:42 +0200

    prevent crawling of 404

--- src/404/views/ViewNotFound.tsx
+++ src/404/views/ViewNotFound.tsx
@@ -3,21 +3,30 @@ import Container from "@mui/material/Container"
 import Paper from "@mui/material/Paper"
 import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
+import { Helmet } from "react-helmet-async"
 import { Link } from "react-router-dom"
 
 
 export default function () {
 	return (
-		<Container maxWidth="sm">
-			<Paper elevation={4} style={{ padding: "1.5rem" }}>
-				<Stack direction="column" spacing={2} alignItems="center">
-					<Typography variant="h3">
-						<b>Seite nicht gefunden</b>
-					</Typography>
+		<>
+			<Helmet>
+				<title>404</title>
+				<meta name="description" content="Seite nicht gefunden" />
+				<meta name="robots" content="noindex" />
+			</Helmet>
 
-					<Button variant="contained" component={Link} to="/">Zur Startseite</Button>
-				</Stack>
-			</Paper>
-		</Container>
+			<Container maxWidth="sm">
+				<Paper elevation={4} style={{ padding: "1.5rem" }}>
+					<Stack direction="column" spacing={2} alignItems="center" component="span" data-nosnippet>
+						<Typography variant="h3">
+							<b>Seite nicht gefunden</b>
+						</Typography>
+
+						<Button variant="contained" component={Link} to="/">Zur Startseite</Button>
+					</Stack>
+				</Paper>
+			</Container>
+		</>
 	)
 }
