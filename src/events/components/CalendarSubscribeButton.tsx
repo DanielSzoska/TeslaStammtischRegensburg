2be77e061c53c36ee4fa831e112623fe3dafa260
Felix Drescher-Hackel <felix@drescher-hackel.de>
2025-09-21 21:57:11 +0200

    simplify calendar link button

--- src/events/components/CalendarSubscribeButton.tsx
+++ src/events/components/CalendarSubscribeButton.tsx
@@ -1,21 +1,18 @@
 import RssFeedIcon from "@mui/icons-material/RssFeed"
 import Button, { ButtonProps } from "@mui/material/Button"
-import { useMemo } from "react"
 import { Link } from "react-router-dom"
 
 
 type Props = Partial<ButtonProps>
 
 export default function ({ ...props }: Props) {
-	const absoluteUrl = useMemo(() => new URL("/download/termine.ics", window.location.href), [ window.location.href ])
-	const webCalUrl = useMemo(() => `https://${absoluteUrl.hostname}${absoluteUrl.pathname}`, [ absoluteUrl ])
-
 	return (
 		<Button
 			variant="contained"
 			color="primary"
 			component={Link}
-			to={webCalUrl}
+			to="/download/termine.ics"
+			target="_blank"
 			startIcon={<RssFeedIcon />}
 			{...props}
 		>
